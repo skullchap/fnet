6d868f2e6fadc2a8d0f0e4f2ae253a372ccdbd52
skullchap <11740883+skullchap@users.noreply.github.com>
2025-03-13 01:45:52 +0400

    compile

--- examples/compile.sh
+++ examples/compile.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-  echo "Provide 'dial' or 'listen'."
+  echo "Provide 'dial' or 'listen'"
   exit 1
 fi
 
 if [ "$1" = "dial" ]; then
   echo "compiling dial"
   cc \
-  -I ./. \
-  fnet.c \
-  examples/dial.c -o examples/dial
+  -I ../. \
+  ../fnet.c \
+  dial.c -o dial
 
 elif [ "$1" = "listen" ]; then
   echo "compiling listen"
   cc \
-  -I ./. \
-  fnet.c \
-  examples/listen.c -o examples/listen
+  -I ../. \
+  ../fnet.c \
+  listen.c -o listen
 
 else
   echo "Allowed args are: dial, listen"
