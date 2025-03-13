2df06d24b72f8bf1aa48b18cca1fed895377a55e
skullchap <11740883+skullchap@users.noreply.github.com>
2025-03-13 16:49:09 +0400

    listen: fnetaccept for udp

--- examples/listen.c
+++ examples/listen.c
@@ -9,7 +9,7 @@ main(int argc, char **argv)
 {
 	NetConn *serv, *client;
 	char buf[256];
-	int isudp= 0;
+	int n, isudp= 0;
 
 	if(argc < 3){
 		printf("forgot to pass proto or address? (e.g. \"%s tcp 127.0.0.1:9999\")\n", argv[0]);
@@ -38,18 +38,22 @@ main(int argc, char **argv)
 		if(!isudp && fork()){
 			fnetclose(client);
 		}else{
-			if(isudp)
-				client = serv;
 			while(1){
-				if(isudp)
+				if(isudp){
+					client = fnetaccept(serv); /* for udp client is the same as server, no need to close it */
 					printf("New Msg (%s <- %s)\n", fnetlocaddr(client), fnetremaddr(client));
-				if(!fgets(buf, sizeof(buf), fnetf(client)))
+				}
+				n = snprintf(buf, sizeof(buf), "%s: ", fnetremaddr(client));
+				if(!fgets(buf+n, sizeof(buf)-n, fnetf(client)))
 					break;
 				fputs(buf, stdout);
 				fflush(stdout);
+				fprintf(fnetf(client), "hello %s!\n", fnetremaddr(client));
+				fflush(fnetf(client));
 			}
 			printf("Closed (%s <- %s)\n", fnetlocaddr(client), fnetremaddr(client));
-			fnetclose(client);
+			if(!isudp)
+				fnetclose(client);
 			return 0;
 		}
 	}
